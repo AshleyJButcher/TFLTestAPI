8a6a805c35c216611adf6d754a2485445f2edfb8
Ashley Butcher <ashley.butcher@csdental.com>
2022-06-29 12:07:57 +0100

    Fixing issues

--- TFLTest/APIRequest.cs
+++ TFLTest/APIRequest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using TFLTest.Models;
 
 namespace TFLTest
 {
@@ -11,22 +12,25 @@ namespace TFLTest
     {
         private static string mbrApplicationID = "";
         private static string mbrApplicationKey = "";
+        private static string URL = "https://api.tfl.gov.uk/Road/";
 
         static HttpClient client = new HttpClient();
         public static async Task<string> GetRoadStatus(string search)
         {
-            string path = "https://api.tfl.gov.uk/Road/" + search + "?app_key=" + mbrApplicationKey + "&app_id=" + mbrApplicationID;
+            string fullURL = $"{URL}{search}?app_key={mbrApplicationKey}&app_id={mbrApplicationID}";
             string output = $"{search} is not a valid road";
-            HttpResponseMessage response = await client.GetAsync(path);
+            HttpResponseMessage response = await client.GetAsync(fullURL);
             if (response.IsSuccessStatusCode)
             {
-                List<RoadDescription> RequestContent = new List<RoadDescription>();
-                RequestContent = await response.Content.ReadAsAsync<List<RoadDescription>>();
-                RoadDescription Result = RequestContent.FirstOrDefault();
-                output = $"The status of the {search} is as follows" + Environment.NewLine + $"Road Status is {Result.statusSeverity}" + Environment.NewLine + $"Road Status Description is {Result.statusSeverityDescription}";
+                List<RoadCorridor>  RequestContent = await response.Content.ReadAsAsync<List<RoadCorridor>>();
+                RoadCorridor Result = RequestContent.FirstOrDefault();
+                output = $"The status of the {search} is as follows" + Environment.NewLine + $"Road Status is {Result.StatusSeverity}" + Environment.NewLine + $"Road Status Description is {Result.StatusSeverityDescription}";
             }
             return output;
         }
 
     }
 }
+
+
+
---
+++ TFLTest/Models/RoadCorridor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFLTest.Models
+{
+    public partial class RoadCorridor
+    {
+        /// <summary>
+        /// The Id of the Corridor e.g. "A406"
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("id", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The display name of the Corridor e.g. "North Circular (A406)". This
+        /// <br/>            may be identical to the Id.
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("displayName", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// The group name of the Corridor e.g. "Central London". Most corridors are not grouped, in which case this field can be null.
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("group", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string Group { get; set; }
+
+        /// <summary>
+        /// Standard multi-mode status severity code
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("statusSeverity", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string StatusSeverity { get; set; }
+
+        /// <summary>
+        /// Description of the status severity as applied to RoadCorridors
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("statusSeverityDescription", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string StatusSeverityDescription { get; set; }
+
+        /// <summary>
+        /// The Bounds of the Corridor, given by the south-east followed by the north-west co-ordinate
+        /// <br/>            pair in geoJSON format e.g. "[[-1.241531,51.242151],[1.641223,53.765721]]"
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("bounds", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string Bounds { get; set; }
+
+        /// <summary>
+        /// The Envelope of the Corridor, given by the corner co-ordinates of a rectangular (four-point) polygon
+        /// <br/>            in geoJSON format e.g. "[[-1.241531,51.242151],[-1.241531,53.765721],[1.641223,53.765721],[1.641223,51.242151]]"
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("envelope", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string Envelope { get; set; }
+
+        /// <summary>
+        /// The start of the period over which status has been aggregated, or null if this is the current corridor status.
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("statusAggregationStartDate", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public System.DateTimeOffset? StatusAggregationStartDate { get; set; }
+
+        /// <summary>
+        /// The end of the period over which status has been aggregated, or null if this is the current corridor status.
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("statusAggregationEndDate", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public System.DateTimeOffset? StatusAggregationEndDate { get; set; }
+
+        /// <summary>
+        /// URL to retrieve this Corridor.
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("url", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string Url { get; set; }
+
+    }
+}
